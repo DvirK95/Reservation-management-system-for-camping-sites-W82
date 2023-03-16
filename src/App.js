35f0a410e78c29aa8184f1644c50696c27a84ca0
DvirK95 <dvirkarl@gmail.com>
2023-03-16 13:58:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import TestApi from "./components/TestApi.js";
+
+function App() {
+  return (
+    <div>
+      <TestApi />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TestApi.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TestApi() {
+  return (
+    <div>
+      <h1>Hello Word</h1>
+    </div>
+  );
+}
+
+export default TestApi;
