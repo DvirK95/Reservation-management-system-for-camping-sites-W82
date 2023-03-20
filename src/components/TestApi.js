5a1e7ef09f791fb8bb4d4ce00864a3d0fd33eaeb
lee <llee1997@gmail.com>
2023-03-20 10:19:48 +0200

    first connect to API

--- src/components/TestApi.js
+++ src/components/TestApi.js
@@ -1,11 +1,42 @@
 import React from "react";
 
-function TestApi() {
-  return (
-    <div>
-      <h1>פאקינג עובד</h1>
-    </div>
-  );
-}
+const axios = require("axios");
+
+const bookingRes = axios.get(
+    "https://lee-cohen.checkfront.com/api/3.0/booking",
+    {
+      headers: {
+        Authorization:
+          "Basic NGQzNmNkOWUzZmI4ZjNhZjgyMjY1NDYwMmM1MGIxMmM4MjE0Yjk3Mjo1ZGI5YTQwYjgyZDI2YjU2YTU1MjcwYTg2ZjQyYWRjZDAxNGEzYWU5NjQyNGNlODMzMDNlNWJiNDExNjIxZTQ3",
+        "content-type": "text/json",
+      },
+    }
+);
+
+bookingRes.then(function(result) {
+  console.log(result.data)
+});
+
+// const getRes = axios.get(
+//     "https://lee-cohen.checkfront.com/api/3.0/category/",
+//     {
+//       headers: {
+//         Authorization:
+//           "Basic NGQzNmNkOWUzZmI4ZjNhZjgyMjY1NDYwMmM1MGIxMmM4MjE0Yjk3Mjo1ZGI5YTQwYjgyZDI2YjU2YTU1MjcwYTg2ZjQyYWRjZDAxNGEzYWU5NjQyNGNlODMzMDNlNWJiNDExNjIxZTQ3",
+//         "content-type": "text/json",
+//       },
+//     }
+// );
+
+// getRes.then(function(result) {
+//   console.log(result.data.category)
+// });
+
+// return (
+//     <div>
+//       <h1> API checkFront test</h1>
+//     </div>
+//   );
+// }
 
 export default TestApi;
