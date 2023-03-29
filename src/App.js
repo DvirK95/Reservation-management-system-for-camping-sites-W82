e016c00e5dfafeb47bc70a95b396c9b84c1a45be
lee <llee1997@gmail.com>
2023-03-29 18:31:01 +0300

    add first page + header + israel map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-import TestApi from "./components/TestApi.js";
+// import TestApi from "./components/Tests/TestApi.js";
+import Header from "./components/UI/Header";
+// import Body from "./components/UI/Body";
+import IsraelMap from './components/Tests/IsraelMap';
 
 function App() {
   return (
     <div>
-      <TestApi />
+      {/* <h1>Let's get start!!!!!</h1> */}
+      <Header />
+      {/* <Body /> */}
+      <IsraelMap />
     </div>
   );
 }
