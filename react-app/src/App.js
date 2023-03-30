0154f483d1e9668b6665cf1f3ec8c74a7496c22b
DvirK95 <dvirkarl@gmail.com>
2023-03-30 13:43:36 +0300

    connect nodeJs server enviorment

---
+++ react-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// import TestApi from "./components/Tests/TestApi.js";
+import Header from "./components/UI/Header";
+// import Body from "./components/UI/Body";
+import IsraelMap from './components/Tests/IsraelMap';
+
+function App() {
+  return (
+    <div>
+      {/* <h1>Let's get start!!!!!</h1> */}
+      <Header />
+      {/* <Body /> */}
+      <IsraelMap />
+    </div>
+  );
+}
+
+export default App;
