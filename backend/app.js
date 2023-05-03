c88d43d339f0114f7ab78720556d6f6221c55dae
DvirK95 <dvirkarl@gmail.com>
2023-05-03 14:38:10 +0300

    update

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,10 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(5000);
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   })
   .catch((error) => {
     console.log(error);
