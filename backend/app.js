572a673725023d11277b9a3b864cda5d91ee39fc
DvirK95 <dvirkarl@gmail.com>
2023-05-03 15:04:58 +0300

    render

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-const express = require("express");
-const { default: mongoose } = require("mongoose");
-const path = require("path");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-=======
 const express = require('express');
 const { default: mongoose } = require('mongoose');
 const dotenv = require('dotenv').config();
->>>>>>> preparebackend2
 
 const app = express();
 
-<<<<<<< HEAD
-=======
 app.use(express.json());
 
->>>>>>> preparebackend2
 // change it on production
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,20 +17,12 @@ app.use((req, res, next) => {
   next();
 });
 
-<<<<<<< HEAD
-const placesRoutes = require("./routes/places");
-app.use("/api/places", placesRoutes);
-
-const siteSRoutes = require("./routes/sites");
-app.use("/api/sites", siteSRoutes);
-=======
 // toRemove in production
 app.use('/post', require('./routes/postData'));
 
 app.use('/places', require('./routes/places-routes'));
 app.use('/sites', require('./routes/site-routes'));
 app.use('/booking', require('./routes/booking-routes'));
->>>>>>> preparebackend2
 
 mongoose
   .connect(process.env.MONGO_DB_PASSWORD, {
