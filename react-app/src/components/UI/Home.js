a72a807759f6963136eaf98b91ca44769f8e4231
DvirK95 <dvirkarl@gmail.com>
2023-06-01 05:39:52 +0300

    30/05

--- react-app/src/components/UI/Home.js
+++ react-app/src/components/UI/Home.js
@@ -1,13 +1,11 @@
 import { useCallback, useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import React from 'react';
-
+import { Spinner } from 'react-bootstrap';
 import './Home.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import IsraelMap from '../IsraelMap/IsraelMap';
 import Legend from '../IsraelMap/Legend';
-import nature from "./nature.png";
+import nature from './nature.png';
 
 function Home() {
   const [sites, setSites] = useState([]);
@@ -33,24 +31,33 @@ function Home() {
   useEffect(() => {
     fetchSites();
   }, [fetchSites]);
-  
+
   const clickHandler = (x, y, id) => {
     mapRef.current.zoomIn(x, y, id);
   };
 
   return (
     <div className="Body">
-      <img src={nature} alt='nature' />
+      <img src={nature} alt="nature" />
       <h1 className="green-title">
         נשמח לארח אתכם ברשת חניוני הלילה של רשות הטבע והגנים !
       </h1>
       <h3>
         להזמנת לינה בחניוני הלילה - <b>לחצו על חניון הלילה המבוקש 👇🏼 </b>
       </h3>
-      <div className="flex-container">
-        <Legend sites={sites} onClick={clickHandler}/>
-        <IsraelMap sites={sites} isLoading={isLoading} ref={mapRef}/>        
-      </div>
+
+      {isLoading ? (
+        <div className="flex-container">
+          <div className="spinner">
+            <Spinner animation="border" />
+          </div>
+        </div>
+      ) : (
+        <div className="flex-container">
+          <Legend sites={sites} onClick={clickHandler} />
+          <IsraelMap sites={sites} isLoading={isLoading} ref={mapRef} />
+        </div>
+      )}
     </div>
   );
 }
