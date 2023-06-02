e2215b60d872956d89d817d655cc6ae723f67dc9
DvirK95 <dvirkarl@gmail.com>
2023-06-02 17:21:43 +0300

    add router to nav header

--- react-app/src/components/UI/Header.js
+++ react-app/src/components/UI/Header.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
 import logo from './logo2.png';
 import './Header.css';
 import Container from 'react-bootstrap/Container';
@@ -11,25 +10,28 @@ import { useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
-  const [isCollapseOpen, setIsCollapseOpen] = useState(false); // Added state variable
+  const [isCollapseOpen, setIsCollapseOpen] = useState(false);
 
   const counter = useSelector((state) => state.cart.counter);
-  const cartIcon = <FontAwesomeIcon icon={faCartShopping} className='cart-icon'/>;
+  const cartIcon = (
+    <FontAwesomeIcon icon={faCartShopping} className="cart-icon" />
+  );
   const dispatch = useDispatch();
 
   return (
     <div>
-      <Navbar 
-        collapseOnSelect expand="lg" 
-        className="navbar-custom" 
-        fixed='top' 
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="navbar-custom"
+        fixed="top"
         onToggle={(isOpen) => setIsCollapseOpen(isOpen)}
       >
         <Container fluid className="d-flex">
-          <Navbar.Brand href="/" className="ms-auto">
+          <Navbar.Brand as={NavLink} to="/" className="ms-auto">
             <img
               className="custom-logo-link"
               src={logo}
@@ -39,21 +41,45 @@ function Header() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav>
-              <Nav.Link href="/Info"> אודות </Nav.Link>
+              <Nav.Link as={NavLink} to="/Info">
+                {' '}
+                אודות{' '}
+              </Nav.Link>
               <NavDropdown title="אתרי קמפינג " id="basic-nav-dropdown">
-                <NavDropdown.Item href="/חניון-לילה-גן-לאומי-אכזיב-וחוף-אכזיב/2">חניון לילה אכזיב</NavDropdown.Item>
-                <NavDropdown.Item href="/חניון-לילה-גן-לאומי-מעיין-חרוד/3">חניון לילה מעיין חרוד</NavDropdown.Item>
-                <NavDropdown.Item href="/חניון-לילה-שמורת-טבע-נחל-עמוד/6">חניון לילה נחל עמוד</NavDropdown.Item>
+                <NavDropdown.Item
+                  as={NavLink}
+                  to="/חניון-לילה-גן-לאומי-אכזיב-וחוף-אכזיב/2"
+                >
+                  חניון לילה אכזיב
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  as={NavLink}
+                  to="/חניון-לילה-גן-לאומי-מעיין-חרוד/3"
+                >
+                  חניון לילה מעיין חרוד
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  as={NavLink}
+                  to="/חניון-לילה-שמורת-טבע-נחל-עמוד/6"
+                >
+                  חניון לילה נחל עמוד
+                </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="/PrivacyPolicy"> מדיניות פרטיות </Nav.Link>
-              <Nav.Link href="/TermsUse"> תנאי שימוש </Nav.Link>
+              <Nav.Link as={NavLink} to="/PrivacyPolicy">
+                {' '}
+                מדיניות פרטיות{' '}
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/TermsUse">
+                {' '}
+                תנאי שימוש{' '}
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
           <Link
             id="cart-button"
             to="cart"
             onMouseOver={() => dispatch(cartActions.showCart(true))}
-            className={isCollapseOpen ? "cart-button-open" : "cart-button"}
+            className={isCollapseOpen ? 'cart-button-open' : 'cart-button'}
           >
             {isCollapseOpen ? null : (
               <>
