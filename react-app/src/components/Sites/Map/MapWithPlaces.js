6cd553fdca17269b35689c30919a52a61a514fb0
DvirK95 <dvirkarl@gmail.com>
2023-06-06 21:46:33 +0300

    zoomSpeed={2}

--- react-app/src/components/Sites/Map/MapWithPlaces.js
+++ react-app/src/components/Sites/Map/MapWithPlaces.js
@@ -33,7 +33,7 @@ function MapWithPlaces({ placesData, mapName }) {
         style={{ overflow: 'hidden', touchAction: 'none' }}
         ref={panZoomRef}
         enableBoundingBox
-        zoomSpeed={1.3}
+        zoomSpeed={2}
         minZoom={1}
         maxZoom={5}
         boundaryRatioVertical={0.8}
